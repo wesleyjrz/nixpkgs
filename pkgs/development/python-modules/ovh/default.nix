08717eb83e7541945f4fab38e2af0c88bba25657
Martin Weinelt <hexa@darmstadt.ccc.de>
2022-04-02 03:02:12 +0200

    python3Packages.ovh: propagate requests

--- pkgs/development/python-modules/ovh/default.nix
+++ pkgs/development/python-modules/ovh/default.nix
@@ -1,19 +1,25 @@
 { lib
 , buildPythonPackage
 , fetchPypi
+, requests
 }:
 
 buildPythonPackage rec {
   pname = "ovh";
   version = "1.0.0";
-
-  # Needs yanc
-  doCheck = false;
+  format = "setuptools";
 
   src = fetchPypi {
     inherit pname version;
     sha256 = "sha256-IQzwu0gwfPNPOLQLCO99KL5Hu2094Y+acQBFXVGzHhU=";
   };
+
+  propagatedBuildInputs = [
+    requests
+  ];
+
+  # Needs yanc
+  doCheck = false;
 
   meta = {
     description = "Thin wrapper around OVH's APIs";
